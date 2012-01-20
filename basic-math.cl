70972225680316c331c0fd732443797871022f46
Václav Šmilauer <eu@doxos.eu>
2012-01-20 16:42:09 +0100

    1. rename "methods" to be consistent 2. implement non-trivial matrix and quaternion funcs 3. add tests

--- basic-math.cl
+++ basic-math.cl
@@ -3,43 +3,111 @@
 
 typedef double3  Vec3;
 typedef double4  Quat;
-typedef double16 Mat33; // wasting some bytes here
+typedef double16 Mat3; // wasting some bytes here
 
 
 // 3x3 matrices are row-major (can be changed as long as accessors are used consistently)
+// getters
+double Mat3_at(Mat3 m, ushort row, ushort col){ return (((double*)(&m))[3*row+col]); }
+Vec3 Mat3_row(Mat3 m, ushort row){ return (Vec3)(Mat3_at(m,row,0),Mat3_at(m,row,1),Mat3_at(m,row,2)); }
+Vec3 Mat3_col(Mat3 m, ushort col){ return (Vec3)(Mat3_at(m,0,col),Mat3_at(m,1,col),Mat3_at(m,2,col)); }
+Vec3 Mat3_diag(Mat3 m){ return (Vec3)(Mat3_at(m,0,0),Mat3_at(m,1,1),Mat3_at(m,2,2)); }
+// setters
+Mat3 Mat3_set(double c00, double c01, double c02, double c10, double c11, double c12, double c20, double c21, double c22){ return (Mat3)(c00,c01,c02, c10,c11,c12, c20,c21,c22, /* tail*/ 0,0,0,0,0,0,0); }
+// (the following code uses accessors, hence no change if layout changes)
+Mat3 Mat3_setCols(Vec3 c0, Vec3 c1, Vec3 c2){ return Mat3_set(c0.s0,c1.s0,c2.s0, c0.s1,c1.s1,c2.s1, c0.s2,c1.s2,c2.s2); }
+Mat3 Mat3_setRows(Vec3 r0, Vec3 r1, Vec3 r2){ return Mat3_set(r0.s0,r0.s1,r0.s2, r1.s0,r1.s1,r1.s2, r2.s0,r2.s1,r2.s2); }
+Mat3 Mat3_setDiag(Vec3 d){ return Mat3_set(d.s0,0,0, 0,d.s1,0, 0,0,d.s2); }
+// const setters
+Mat3 Mat3_zero(){ return (Mat3)(0.); }
+Mat3 Mat3_identity(){ return Mat3_setDiag((Vec3)1.); }
 
-// get element
-double m33at(Mat33 m, ushort row, ushort col){ return (((double*)(&m))[3*row+col]); }
-// set the whole matrix from row-ordered entries
-Mat33 m33set(double c00, double c01, double c02, double c10, double c11, double c12, double c20, double c21, double c22){ return (Mat33)(c00,c01,c02, c10,c11,c12, c20,c21,c22, /* tail*/ 0,0,0,0,0,0,0); }
-
-
-// the following code uses accessors, hence no change if layout changes
-// set matrix from columns as vectors
-Mat33 m33setCols(Vec3 c0, Vec3 c1, Vec3 c2){ return m33set(c0.s0,c1.s0,c2.s0, c0.s1,c1.s1,c2.s1, c0.s2,c1.s2,c2.s2); }
-// set matrix from rows as vectors
-Mat33 m33setRows(Vec3 r0, Vec3 r1, Vec3 r2){ return m33set(r0.s0,r0.s1,r0.s2, r1.s0,r1.s1,r1.s2, r2.s0,r2.s1,r2.s2); }
-// get row
-Vec3  m33row(Mat33 m, ushort row){ return (Vec3)(m33at(m,row,0),m33at(m,row,1),m33at(m,row,2)); }
-// get columns
-Vec3  m33col(Mat33 m, ushort col){ return (Vec3)(m33at(m,0,col),m33at(m,1,col),m33at(m,2,col)); }
-// get diagonal
-Vec3  m33diag(Mat33 m){ return (Vec3)(m33at(m,0,0),m33at(m,1,1),m33at(m,2,2)); }
-// transpose
-Mat33 m33trans(Mat33 m){ return m33setRows(m33col(m,0),m33col(m,1),m33col(m,2)); }
-// http://en.wikipedia.org/wiki/Outer_product
-Mat33 outerVV3(Vec3 a, Vec3 b){ return m33set(a.s0*b.s0,a.s0*b.s1,a.s0*b.s2,a.s1*b.s0,a.s1*b.s1,a.s1*b.s2,a.s2*b.s0,a.s2*b.s1,a.s2*b.s2); }
+// operations
+Mat3 Mat3_transpose(Mat3 m){ return Mat3_setRows(Mat3_col(m,0),Mat3_col(m,1),Mat3_col(m,2)); }
+double Mat3_trace(Mat3 m){ return Mat3_at(m,0,0)+Mat3_at(m,1,1)+Mat3_at(m,2,2); }
+// matrix-vector multiply
+Vec3 Mat3_multV(Mat3 a, Vec3 b){	return (Vec3)(dot(Mat3_row(a,0),b),dot(Mat3_row(a,1),b),dot(Mat3_row(a,2),b)); }
 // matrix-matrix multiply
-Mat33 mulMM3(Mat33 a, Mat33 b){
-	Vec3 ar0=m33row(a,0),ar1=m33row(a,1),ar2=m33row(a,2);
-	Vec3 bc0=m33col(b,0),bc1=m33col(b,1),bc2=m33col(b,2);
-	return m33set(
+Mat3 Mat3_multM(Mat3 a, Mat3 b); // impl below
+// Gram-Schmidt orthonormalization with stable 0th column
+Mat3 Mat3_orthonorm_c0(Mat3);  // impl below
+
+// http://en.wikipedia.org/wiki/Outer_product
+Mat3 Vec3_outer(Vec3 a, Vec3 b){ return Mat3_set(a.s0*b.s0,a.s0*b.s1,a.s0*b.s2,a.s1*b.s0,a.s1*b.s1,a.s1*b.s2,a.s2*b.s0,a.s2*b.s1,a.s2*b.s2); }
+
+Quat Quat_identity(){ return (Quat)(0,0,0,1); }
+Quat Quat_conjugate(Quat q){ return (Quat)(-q.x,-q.y,-q.z,q.w); }
+Quat Mat3_toQuat(Mat3 rot); // impl below
+Vec3 Quat_rotate(Quat q, Vec3 v);
+
+Vec3 Vec3_unitX(){ return (Vec3)(1,0,0); }
+Vec3 Vec3_unitY(){ return (Vec3)(0,1,0); }
+Vec3 Vec3_unitZ(){ return (Vec3)(0,0,1); }
+Vec3 Vec3_unit(ushort i){ return (i==0?Vec3_unitX():(i==1?Vec3_unitY():Vec3_unitZ())); }
+Vec3 Vec3_zero() { return (Vec3)(0,0,0); }
+
+double Vec3_sqNorm(Vec3 v){ return dot(v,v); }
+
+/* non-trivial implementations */
+
+Mat3 Mat3_multM(Mat3 a, Mat3 b){
+	Vec3 ar0=Mat3_row(a,0),ar1=Mat3_row(a,1),ar2=Mat3_row(a,2);
+	Vec3 bc0=Mat3_col(b,0),bc1=Mat3_col(b,1),bc2=Mat3_col(b,2);
+	return Mat3_set(
 		dot(ar0,bc0),dot(ar0,bc1),dot(ar0,bc2),
 		dot(ar1,bc0),dot(ar1,bc1),dot(ar1,bc2),
 		dot(ar2,bc0),dot(ar2,bc1),dot(ar2,bc2)
 	);
 }
-// matrix-vector multiply
-Vec3 mulMV3(Mat33 a, Vec3 b){	return (Vec3)(dot(m33row(a,0),b),dot(m33row(a,1),b),dot(m33row(a,2),b)); }
 
+// following routines implemented following:
+// eigen3/Eigen/src/Geometry/Quaternion.h: quaternionbase_assign_impl
+Mat3 Quat_toMat3(Quat q){
+	double twx=2*q.w*q.x, twy=2*q.w*q.y, twz=2*q.w*q.z,
+		txx=2*q.x*q.x, txy=2*q.x*q.y, txz=2*q.x*q.z,
+		tyy=2*q.y*q.y, tyz=2*q.y*q.z, tzz=2*q.z*q.z;
+	return Mat3_set(
+		1-(tyy+tzz),txy-twz,txz+twy,
+		txy+twz,1-(txx+tzz),tyz-twx,
+		txz-twy,tyz+twx,1-(txx+tyy)
+	);
+}
+/* the matrix must be orthonormal! */
+Quat Mat3_toQuat(Mat3 rot){
+	double t=Mat3_trace(rot);
+	Quat q;
+	if(t>0){
+		t=sqrt(t+1);
+		q.w=.5*t;
+		q.x=(Mat3_at(rot,2,1)-Mat3_at(rot,1,2))*t;
+		q.y=(Mat3_at(rot,0,2)-Mat3_at(rot,2,0))*t;
+		q.z=(Mat3_at(rot,1,0)-Mat3_at(rot,0,1))*t;
+	} else {
+		ushort i=0;
+		if(Mat3_at(rot,1,1)>Mat3_at(rot,0,0)) i=0;
+		if(Mat3_at(rot,2,2)>Mat3_at(rot,i,i)) i=2;
+		ushort j=(i+1)%3, k=(i+2)%3;
+		t=sqrt(Mat3_at(rot,i,i)-Mat3_at(rot,j,j)-Mat3_at(rot,k,k)+1.);
+		((double*)&q)[i]=.5*t;
+		t=.5/t;
+		q.w=(Mat3_at(rot,k,j)-Mat3_at(rot,j,k))*t;
+		((double*)&q)[j]=(Mat3_at(rot,j,i)+Mat3_at(rot,i,j));
+		((double*)&q)[k]=(Mat3_at(rot,k,i)+Mat3_at(rot,i,k));
+	}
+	return q;
+}
+
+Mat3 Mat3_orthonorm_c0(Mat3 m){
+	Vec3 x=Mat3_col(m,0), y=Mat3_col(m,1);
+	x=normalize(x);
+	y=y-x*dot(x,y);
+	y=normalize(y);
+	return Mat3_setCols(x,y,cross(x,y));
+}
+
+
+Vec3 Quat_rotate(Quat q, Vec3 v){
+	Vec3 uv=2*cross(q.xyz,v);
+	return v+q.w*uv+cross(q.xyz,uv);
+}
 
--- test0.py
+++ test0.py
@@ -5,12 +5,35 @@ import numpy.linalg as la
 import collections
 import sys
 
-count=100
-align=16 # alignment for Mat33
+count=2
+align=16 # alignment for Mat3
 N=count*16
+
+
+# convert flat buffer to numpy tupe, according to type 
+def toNumpy(typeName,vec,i):
+	if typeName==None: return None
+	if typeName=='Vec3': return numpy.matrix(vec[i*4:i*4+3].reshape((3,1)))
+	elif typeName=='Mat3': return numpy.matrix(vec[i*16:i*16+9].reshape((3,3)))
+	elif typeName=='double': return float(vec[i])
+	elif typeName=='Quat': return Quaternion(vec[i*4+3],vec[i*4],vec[i*4+1],vec[i*4+2])
+
+def orthonorm_c0(m):
+	x=m[:,0]; y=m[:,1]
+	x/=la.norm(x)
+	y-=x*numpy.dot(x.T,y)
+	y/=la.norm(y)
+	m[:,0]=x; m[:,1]=y; m[:,2]=numpy.cross(x.T,y.T).T
+	return m
+
 
 a=numpy.random.rand(N).astype(numpy.float64)
 b=numpy.random.rand(N).astype(numpy.float64)
+aRot=numpy.zeros(N).astype(numpy.float64)
+for i in range(0,count):
+	m=numpy.matrix(numpy.random.random((3,3)))
+	m=orthonorm_c0(m)
+	aRot[align*i:align*i+9]=m.reshape((1,9))
 
 ctx=cl.create_some_context(interactive=False)
 queue=cl.CommandQueue(ctx)
@@ -18,6 +41,7 @@ mf=cl.mem_flags
 
 aBuf=cl.Buffer(ctx,mf.READ_ONLY | mf.COPY_HOST_PTR,hostbuf=a)
 bBuf=cl.Buffer(ctx,mf.READ_ONLY | mf.COPY_HOST_PTR,hostbuf=b)
+aRotBuf=cl.Buffer(ctx,mf.READ_ONLY | mf.COPY_HOST_PTR,hostbuf=aRot)
 cBuf=cl.Buffer(ctx,mf.WRITE_ONLY,b.nbytes)
 
 CLTest=collections.namedtuple('CLTest',['name','outType','inTypes','clCode','numpyFunc'])
@@ -30,53 +54,68 @@ CLTest=collections.namedtuple('CLTest',['name','outType','inTypes','clCode','num
 # numpyFunc: lambda to be called on inputs to get numpy answer for comparison
 
 tests=[
-	# vector built-ins
+	# vector
+	CLTest('Vec3_unitX','Vec3',(None,None),'c=Vec3_unitX();',lambda a,b: numpy.matrix([[1,0,0]]).T),
+	CLTest('Vec3_unitY','Vec3',(None,None),'c=Vec3_unitY();',lambda a,b: numpy.matrix([[0,1,0]]).T),
+	CLTest('Vec3_unitZ','Vec3',(None,None),'c=Vec3_unitZ();',lambda a,b: numpy.matrix([[0,0,1]]).T),
+	CLTest('Vec3_zero','Vec3',(None,None),'c=Vec3_zero();',lambda a,b: numpy.matrix([[0,0,0]]).T),
+	CLTest('Vec3+Vec3','Vec3',('Vec3','Vec3'),'c=a+b;',lambda a,b: numpy.array([a])+numpy.array([b])),
+	CLTest('Vec3*double','Vec3',('Vec3','double'),'c=a*b;',lambda a,b: numpy.array([a])*b),
 	# matrix built-ins
-	CLTest('matrix addition','Mat33',('Mat33','Mat33'),'c[gid]=a[gid]+b[gid]',lambda a,b: a+b),
-	CLTest('matrix identity','Mat33',('Mat33','Mat33'),'c[gid]=a[gid]',lambda a,b: a),
-	CLTest('scalar-matrix product','Mat33',('double','Mat33'), 'c[gid]=a[gid]*b[gid];',lambda a,b: a*b),
-	CLTest('matrix-scalar product','Mat33',('Mat33','double'), 'c[gid]=a[gid]*b[gid];',lambda a,b: a*b),
+	CLTest('Mat3+Mat3','Mat3',('Mat3','Mat3'),'c=a+b',lambda a,b: a+b),
+	CLTest('Mat3=Mat3','Mat3',('Mat3','Mat3'),'c=a',lambda a,b: a),
+	CLTest('double*Mat3','Mat3',('double','Mat3'), 'c=a*b;',lambda a,b: a*b),
+	CLTest('Mat3*double','Mat3',('Mat3','double'), 'c=a*b;',lambda a,b: a*b),
 	# matrix trivial
-	CLTest('set rows','Mat33',(None,None),'c[gid]=m33setRows((Vec3)0,(Vec3)(1),(Vec3)2);',lambda a,b: numpy.matrix([[0,0,0],[1,1,1],[2,2,2]])),
-	CLTest('set cols','Mat33',(None,None),'c[gid]=m33setCols((Vec3)0,(Vec3)(1),(Vec3)2);',lambda a,b: numpy.matrix([[0,0,0],[1,1,1],[2,2,2]]).T),
-	CLTest('set components','Mat33',(None,None),'c[gid]=m33set(0,0.1,0.2,1.0,1.1,1.2,2.0,2.1,2.2);',lambda a,b: numpy.matrix([[0,.1,.2],[1.,1.1,1.2],[2.,2.1,2.2]])),
+	CLTest('Mat3_setRows','Mat3',(None,None),'c=Mat3_setRows((Vec3)0,(Vec3)(1),(Vec3)2);',lambda a,b: numpy.matrix([[0,0,0],[1,1,1],[2,2,2]])),
+	CLTest('Mat3_setCols','Mat3',(None,None),'c=Mat3_setCols((Vec3)0,(Vec3)(1),(Vec3)2);',lambda a,b: numpy.matrix([[0,0,0],[1,1,1],[2,2,2]]).T),
+	CLTest('Mat3_set','Mat3',(None,None),'c=Mat3_set(0,0.1,0.2,1.0,1.1,1.2,2.0,2.1,2.2);',lambda a,b: numpy.matrix([[0,.1,.2],[1.,1.1,1.2],[2.,2.1,2.2]])),
 	# matrix non-trivial
-	CLTest('transpose','Mat33',('Mat33',None),'c[gid]=m33trans(a[gid])',lambda a,b: a.T),
-	CLTest('outer product','Mat33',('Vec3','Vec3'),  'c[gid]=outerVV3(a[gid],b[gid])',lambda a,b: numpy.outer(a,b)),
-	CLTest('matrix-matrix product','Mat33',('Mat33','Mat33'), 'c[gid]=mulMM3(a[gid],b[gid])',lambda a,b: a*b),
-	CLTest('matrix-vector product','Vec3',('Mat33','Vec3'), 'c[gid]=mulMV3(a[gid],b[gid])',lambda a,b: a*b),
+	CLTest('Mat3_transpose','Mat3',('Mat3',None),'c=Mat3_transpose(a)',lambda a,b: a.T),
+	CLTest('Vec3_outer','Mat3',('Vec3','Vec3'),  'c=Vec3_outer(a,b)',lambda a,b: numpy.outer(a,b)),
+	CLTest('Mat3_multM','Mat3',('Mat3','Mat3'), 'c=Mat3_multM(a,b)',lambda a,b: a*b),
+	CLTest('Mat3_multV','Vec3',('Mat3','Vec3'), 'c=Mat3_multV(a,b)',lambda a,b: a*b),
+	CLTest('Mat3_orthonorm_c0','Mat3',('Mat3',None),'c=Mat3_orthonorm_c0(a)',lambda a,b: orthonorm_c0(a)),
+	#CLTest('Mat3_toQuat [rot]','Quat',('Mat3',None),'c=Mat3_toQuat(a)',lambda a,b: Quaternion(Matrix3(a)))
 ]
 
-# convert flat buffer to numpy tupe, according to type 
-def toNumpy(typeName,vec,i):
-	if typeName==None: return None
-	if typeName=='Vec3': return numpy.matrix(vec[i*4:i*4+3].reshape((3,1)))
-	elif typeName=='Mat33': return numpy.matrix(vec[i*16:i*16+9].reshape((3,3)))
-	elif typeName=='double': return float(vec[i])
+#tests=[tests[-1]]
+
+
+
+
 
 for test in tests:
 	print 20*'=',test.name,20*'='
-	prg=cl.Program(ctx,'''
-		#include"basic-math.cl"
-		kernel void test(global %s *c, global const %s *a, global const %s *b){ int gid=get_global_id(0); %s; }
-	'''%(test.outType,test.inTypes[0] if test.inTypes[0] else 'Vec3',test.inTypes[1] if test.inTypes[1] else 'Vec3',test.clCode)).build(options="-I.")
-	prg.test(queue,(a.shape[0]/align,),None,cBuf,aBuf,bBuf) # we pass uselessly aBuf, bBuf even if they are unused
+	rotMatAsInput='[rot]' in test.name
+	src='''
+		#include<basic-math.cl>
+		kernel void test(global {typeC} *cc, global const {typeA} *aa, global const {typeB} /**/ *bb){{ int gid=get_global_id(0); {typeA} a=aa[gid]; {typeB} b=bb[gid]; {typeC} c; {clCode}; cc[gid]=c; }}
+	'''.format(typeA=(test.inTypes[0] if test.inTypes[0] else 'double'),typeB=(test.inTypes[1] if test.inTypes[1] else 'double'),typeC=test.outType,clCode=test.clCode)	
+	prg=cl.Program(ctx,src).build(options="-I.")
+	prg.test(queue,(a.shape[0]/align,),None,cBuf,aBuf if not rotMatAsInput else aRotBuf,bBuf) # we pass uselessly aBuf, bBuf even if they are unused
 	c=numpy.empty_like(a) # result will be copied in here
 	cl.enqueue_read_buffer(queue,cBuf,c).wait() # compute, wait and copy back
 	# go through all results, compare with numpy
 	for i in range(0,N/align):
-		A,B=toNumpy(test.inTypes[0],a,i),toNumpy(test.inTypes[1],b,i)
+		A,B=toNumpy(test.inTypes[0],a if not rotMatAsInput else aRot,i),toNumpy(test.inTypes[1],b,i)
 		C=toNumpy(test.outType,c,i)
 		D=test.numpyFunc(A,B) # call numpy on the args
 		if numpy.all(C==D):   # all elements exactly the same
 			sys.stdout.write('.')
 		else:
-			print 20*'@','error'
-			if test.inTypes[0]: print 'input A:\n',A
-			if test.inTypes[1]: print 'input B:\n',B
-			print 'CL raw:\n',c
-			print 'CL:\n',C
-			print 'numpy:\n',D
+			relErr=la.norm(C-D)/la.norm(C)
+			if relErr<1e-14:
+				sys.stdout.write(':')
+			elif relErr<1e-8:
+				sys.stdout.write('[%g]'%relErr)
+			else:
+				print 20*'@','error'
+				if test.inTypes[0]: print 'input A:\n',A
+				if test.inTypes[1]: print 'input B:\n',B
+				#print 'CL raw:\n',c
+				print 'CL:\n',C
+				print 'numpy:\n',D
 	print 
 		
 
